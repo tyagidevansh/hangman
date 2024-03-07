3f057b694cebba447e7e989c1da4f434968b668f
tyagidevansh <devanshtyagi0607@gmail.com>
2024-03-07 19:11:29 +0530

    letters appear + winning condition, images downloaded not displayed

--- hangman.js
+++ hangman.js
@@ -1,5 +1,6 @@
 let words = [];
 let currentWord = '';
+let displayWord = '';
 
 function fetchWords() {
     fetch('words.csv')
@@ -10,7 +11,6 @@ function fetchWords() {
                 const lineWords = line.split(',');
                 words = words.concat(lineWords);
             });
-            // Call getNewWord after words are fetched
             getNewWord();
         })
         .catch(error => {
@@ -21,20 +21,53 @@ function fetchWords() {
 function getNewWord() {
     const randomIndex = Math.floor(Math.random() * words.length);
     const randomWord = words[randomIndex];
-    console.log('Random Word: ', randomWord);
     currentWord = randomWord;
-    updateWordDisplay(); 
+    currentWord = currentWord.toUpperCase().trim();
+    console.log('Current Word: ', currentWord);
+    displayDashes(); 
 }
 
-function updateWordDisplay() {
-    const dashes = currentWord.split('').map(char => '__').join(' ');
+function displayDashes() {
+    // let dashes = []
+    // for (let i = 0; i < currentWord.length - 1; i++) {
+    //     dashes[i] = '_'; 
+    // }
+    // dashes = dashes.join('');
     const wordDisplay = document.getElementById('wordDisplay');
-    wordDisplay.textContent = dashes;
+    //displayWord = dashes;
+    displayWord = "_".repeat(currentWord.length)
+    wordDisplay.textContent = displayWord;
+}
+
+function updateText(keyText) {
+    let displayWordArray = displayWord.split('');
+    for (let i = 0; i < currentWord.length; i++) {
+        if (currentWord.charAt(i) == keyText) {
+            displayWordArray[i] = keyText;
+        }
+    }
+    displayWord = displayWordArray.join('');
+    console.log(displayWord);
+    wordDisplay.textContent = displayWord;
+}
+
+function checkWin() {
+    if (displayWord === currentWord) {
+        console.log("You Win"); 
+        setTimeout(() => {
+            location.reload();
+        }, 3000);    
+    }
 }
 
 document.addEventListener('DOMContentLoaded', fetchWords);
 
-// const keys = document.querySelectorAll('.key');
-// keys.forEach(key => {
-//     key.addEventListener('click', getNewWord);
-// });
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('click', () => {
+        const keyText = key.textContent;
+        console.log(keyText);
+        updateText(keyText);
+        checkWin();
+    });
+});
